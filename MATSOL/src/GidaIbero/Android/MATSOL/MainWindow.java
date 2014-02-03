fdf42e7065816e174d2eb27859d7cabd03a71c08
Santiago Torres <torresariass@gmail.com>
2014-02-02 22:27:05 -0500

    Updated main window with a callback for the about button About button now spawns the about activity when clicked

--- MATSOL/src/GidaIbero/Android/MATSOL/MainWindow.java
+++ MATSOL/src/GidaIbero/Android/MATSOL/MainWindow.java
@@ -1,10 +1,13 @@
 package GidaIbero.Android.MATSOL;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainWindow extends Activity
 {
@@ -15,6 +18,8 @@ public class MainWindow extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    // Action bar methods
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
       // Inflate the menu items for use in the action bar
@@ -22,4 +27,23 @@ public class MainWindow extends Activity
       inflater.inflate(R.menu.main_activity_actions, menu);
       return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      // Handle presses on the action bar items
+      switch (item.getItemId()) {
+        case R.id.action_about:
+          openAbout();
+          return true;
+        default:
+          return super.onOptionsItemSelected(item);
+      }
+    }
+    // Activity Transition methods
+    private boolean openAbout(){
+      Intent intent = new Intent(this, About.class);
+      startActivity(intent);
+      return true;
+    }
 }
+
