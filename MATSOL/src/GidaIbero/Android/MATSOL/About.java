6c019b354b49286f71c0dc4e0677edc25f1057ea
Santiago Torres <torresariass@gmail.com>
2014-02-02 22:30:25 -0500

    Added the class for the about Activity.

---
+++ MATSOL/src/GidaIbero/Android/MATSOL/About.java
@@ -0,0 +1,27 @@
+package GidaIbero.Android.MATSOL;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class About extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu items for use in the action bar
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.main_activity_actions, menu);
+      return super.onCreateOptionsMenu(menu);
+    }
+}
