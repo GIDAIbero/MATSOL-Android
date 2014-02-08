12911ae3954bab9543b540fd5a428cc1e9f4f74c
Santiago Torres <torresariass@gmail.com>
2014-02-07 22:49:34 -0500

    Added the callback for two buttons Linear equation system and determinant buttons now launch a dialog to select the size of the matrix

--- MATSOL/src/GidaIbero/Android/MATSOL/MainWindow.java
+++ MATSOL/src/GidaIbero/Android/MATSOL/MainWindow.java
@@ -9,6 +9,13 @@ import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.view.View;
 
+// Dialog that lets the select user the size of the matrix
+import GidaIbero.Android.MATSOL.MatrixDimensionPicker;
+import android.app.Dialog;
+import android.app.DialogFragment;
+//for debugging purposes only
+import android.widget.Toast;
+
 public class MainWindow extends Activity
 {
     /** Called when the activity is first created. */
@@ -39,11 +46,46 @@ public class MainWindow extends Activity
           return super.onOptionsItemSelected(item);
       }
     }
+
+    ////////////////////////////////
     // Activity Transition methods
+    // /////////////////////////////
+
+    // openAbout method:
+    //  a simple 0 argument callback for the about button, launches the
+    //  about activity.
     private boolean openAbout(){
       Intent intent = new Intent(this, About.class);
       startActivity(intent);
       return true;
     }
+  
+    // matrixDimensionPicker
+    //  the callback for the determinant and linear equation system,
+    //  preparses information about the caller and displays a box to the user
+    //  requesting the size of the matrix to calculate
+    public void matrixDimensionPicker(View view){
+      Toast toast; 
+      DialogFragment dialog;
+
+      int callerId = view.getId();
+      // Should identify the caller
+      if(callerId==R.id.matrix_button){
+        toast = Toast.makeText(this,"linear equation system button pressed",1);
+        // TODO: this should be a localizable constant
+        dialog = new MatrixDimensionPicker(callerId,"choose the size of the equation");
+      }
+      else{
+        toast = Toast.makeText(this,"determinant button pressed",1);
+        // TODO: this should also be a localizable constant
+        dialog = new MatrixDimensionPicker(callerId,"choose the size of the determinant");
+      }
+      // Should create intent information and pass it to the dialog
+
+      // Should display the dialog
+      dialog.show(getFragmentManager(), "missiles");
+      toast.show();
+    }
+  
 }
 
