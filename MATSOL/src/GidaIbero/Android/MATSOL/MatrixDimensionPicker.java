74c29920687696489bb5c48afb4245914248b8dd
Santiago Torres <torresariass@gmail.com>
2014-02-10 02:35:37 -0500

    Fixed a Memory management issue. Fixed a memory management issue that resulted in an error with the new localization-able string

--- MATSOL/src/GidaIbero/Android/MATSOL/MatrixDimensionPicker.java
+++ MATSOL/src/GidaIbero/Android/MATSOL/MatrixDimensionPicker.java
@@ -7,17 +7,20 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
-
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.view.View;
 
 public class MatrixDimensionPicker extends DialogFragment {
   int target;
   String message;
-  
+  SeekBar seekBar;
+  TextView textView;
+  View layout;
   public MatrixDimensionPicker(int target, String message){
     super();
     this.target = target;
-    this.message = message;
-    
+    this.message = new String(message);
   }
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -34,14 +37,36 @@ public class MatrixDimensionPicker extends DialogFragment {
     LayoutInflater inflater = getActivity().getLayoutInflater();
     // Inflate and set the layout for the dialog
     // Pass null as the parent view because its going in the dialog layout
-    builder.setView(inflater.inflate(R.layout.matrix_picker_dialog, null));
+    layout = inflater.inflate(R.layout.matrix_picker_dialog,null);
+    
+    //add listener to the actionBar
+    seekBar = (SeekBar)layout.findViewById(R.id.matrix_seek_bar);
+    textView = (TextView)(layout.findViewById(R.id.matrix_seek_display));
+    seekBar.setOnSeekBarChangeListener(new seekBarListener(textView));
+    
+    builder.setView(layout);
+
     // Create the AlertDialog object and return it
+    //
+
     return builder.create();
   }
 
   private class dialogListener implements DialogInterface.OnClickListener{
     public void onClick(DialogInterface dialog, int id){
-
+      
     }
+  }
+  private class seekBarListener implements SeekBar.OnSeekBarChangeListener{
+    TextView textView;
+    public seekBarListener(TextView textView){
+      super();
+      this.textView = textView;
+    }
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+      this.textView.setText(String.format("%d",progress));
+    }
+    public void onStartTrackingTouch(SeekBar seekBar){}
+    public void onStopTrackingTouch(SeekBar seekBar){}
   }
 }
