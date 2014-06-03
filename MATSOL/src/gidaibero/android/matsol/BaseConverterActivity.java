8211ca660ffff2260e5e561700b18161c19589fb
Santiago Torres <torresariass@gmail.com>
2014-06-02 22:49:18 -0400

    Added base files for the BaseConverter activity
    These are the required files to provide the base converter activity.
    Navigation is already connected throught the MainWindow class and the
    manifest. The class is, however, an empty placeholder to be filled in.
    
    AndroidManifest.xml:
        Added the activity tag for this class
    
    res/layout/base_converter:
        created empty placeholder
    
    res/layout/main.xml:
        added the button and the callback to push the new activity
    
    res/values/strings.xml:
        Added the constant to show in the status bar for the new activity
    
    src/gidaibero/android/matsol/BaseConverterActivity.java:
        Added base class
    
    src/gidaibero/android/matsol/MainWindow.java:
        Added the connecting code.

---
+++ MATSOL/src/gidaibero/android/matsol/BaseConverterActivity.java
@@ -0,0 +1,33 @@
+/***************************
+You may use, distribute and copy MATSOL for Android under the terms of GNU
+General Public License version 3, which is displayed below.
+****************************/
+
+package gidaibero.android.matsol;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class BaseConverterActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_converter);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
--- MATSOL/src/gidaibero/android/matsol/MainWindow.java
+++ MATSOL/src/gidaibero/android/matsol/MainWindow.java
@@ -25,6 +25,7 @@ import android.widget.Toast;
 
 public class MainWindow extends Activity implements MatrixDimensionPicker.DialogListener
 {
+
     public final static String MATRIX_TARGET = 
       "gidaibero.android.matsol.matrix_target";
     public final static String MATRIX_SIZE = 
@@ -41,92 +42,131 @@ public class MainWindow extends Activity implements MatrixDimensionPicker.Dialog
     // Action bar methods
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-      // Inflate the menu items for use in the action bar
-      MenuInflater inflater = getMenuInflater();
-      inflater.inflate(R.menu.main_activity_actions, menu);
-      return super.onCreateOptionsMenu(menu);
+
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-      //Toast.makeText(this,"wtf",1).show();
-      // Handle presses on the action bar items
-      switch (item.getItemId()) {
-        case R.id.action_about:
-          openAbout();
-          return true;
-        default:
-          return super.onOptionsItemSelected(item);
-      }
+
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+
+            case R.id.action_about:
+
+                openAbout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+          }
+
     }
 
-    ////////////////////////////////
-    // Activity Transition methods
-    // /////////////////////////////
+    /*
+     * ===========================
+     * Activity Transition methods
+     * ---------------------------
+     */
 
-    // openAbout method:
-    //  a simple 0 argument callback for the about button, launches the
-    //  about activity.
-    private boolean openAbout(){
-      Intent intent = new Intent(this, About.class);
-      startActivity(intent);
-      return true;
-    }
-  
-    // matrixDimensionPicker
-    //  the callback for the determinant and linear equation system,
-    //  preparses information about the caller and displays a box to the user
-    //  requesting the size of the matrix to calculate
-    public void showMatrixDimensionPicker(View view){
-      DialogFragment dialog;
-      int callerId = view.getId();
-      // Should identify the caller
-      if(callerId==R.id.matrix_button){
-        dialog = new MatrixDimensionPicker(callerId,
-           this.getString(R.string.matrix_size_message_picker_dialog));
-        
-        dialog.show(getFragmentManager(), "matrix_size_dialog");
-      }
-      else{
-        dialog = new MatrixDimensionPicker(callerId,
-            this.getString(R.string.determinant_message_picker_dialog));
-        dialog.show(getFragmentManager(), "determinant_size_dialog");
-      }
-      // Should create intent information and pass it to the dialog
-      
-      // Should display the dialog
+    /* openAbout method:
+     *  a simple 0 argument callback for the about button, launches the
+     *  about activity.
+     */
+    private boolean openAbout() {
+
+        Intent intent = new Intent(this, About.class);
+
+        startActivity(intent);
+        return true;
     }
   
-    
+    /* showMatrixDimensionPicker
+     *  the callback for the determinant and linear equation system,
+     *  preparses information about the caller and displays a box to the user
+     * requesting the size of the matrix to calculate
+     */
+    public void showMatrixDimensionPicker(View view) {
+
+        DialogFragment dialog;
+        int callerId = view.getId();
+
+        // Should identify the caller
+        if(callerId==R.id.matrix_button){
+
+            dialog = new MatrixDimensionPicker(callerId,
+               this.getString(R.string.matrix_size_message_picker_dialog));
+            
+            dialog.show(getFragmentManager(), "matrix_size_dialog");
+
+        } else {
+
+            dialog = new MatrixDimensionPicker(callerId,
+                this.getString(R.string.determinant_message_picker_dialog));
+            dialog.show(getFragmentManager(), "determinant_size_dialog");
+
+        }
+          // TODO Should create intent information and pass it to the dialog
+          
+          // Should display the dialog
+    }
+  
+    /* pushBaseConverterActivity method
+     *
+     * shows the base converter activity, this doesn't need any special
+     * wrapping
+     */
+    public void pushBaseConverterActivity(View view) {
+
+        // TODO: show something else instead of the About class.
+        Intent intent = new Intent(this, BaseConverterActivity.class);
+
+        startActivity(intent);
+
+    } 
 
 
-    ////////////////////////////////////////////////////
-    // dialog fragment callbacks
-    // ////////////////////////////////////////////////
+    /*
+     * ==================================
+     * Dialog fragment callbacks
+     * ---------------------------------
+     */
 
-    // The dialog fragment receives a reference to this Activity through the
-    // Fragment.onAttach() callback, which it uses to call the following methods
-    // defined by the NoticeDialogFragment.NoticeDialogListener interface
     @Override
     public void onDialogPositiveClick(DialogFragment dialog) {
-      // User touched the dialog's positive button
-      if(dialog instanceof MatrixDimensionPicker){
-        MatrixDimensionPicker matrix_dialog = (MatrixDimensionPicker) dialog;
-        int target = matrix_dialog.getTarget();
 
-        Intent intent = new Intent(this,  MatrixInputActivity.class);
-        // bundle the id number of the button that caused it as the selected operation
-        intent.putExtra(MATRIX_TARGET, target + "");
+          // User touched the dialog's positive button
+          if(dialog instanceof MatrixDimensionPicker){
 
-        // bundle the size of the matrix
-        intent.putExtra(MATRIX_SIZE, matrix_dialog.getSize() + "");
-        startActivity(intent);
-      }
+                MatrixDimensionPicker matrix_dialog =
+                    (MatrixDimensionPicker)dialog;
+
+                int target = matrix_dialog.getTarget();
+
+                Intent intent = new Intent(this,  MatrixInputActivity.class);
+
+                // bundle the id number of the button that caused it as the
+                // selected operation
+                intent.putExtra(MATRIX_TARGET, target + "");
+
+                // bundle the size of the matrix
+                intent.putExtra(MATRIX_SIZE, matrix_dialog.getSize() + "");
+                startActivity(intent);
+
+          }
     }
     
     @Override
     public void onDialogNegativeClick(DialogFragment dialog) {
+
       // User touched the dialog's negative button
+      
     }
 }
 
